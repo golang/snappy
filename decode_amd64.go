857ad66e003f466b64f9055849bab122efd4c87a
Nigel Tao <nigeltao@golang.org>
2016-04-04 10:05:40 +1000

    Add gc build tag for the asm code.
    Fixes #27.

--- decode_amd64.go
+++ decode_amd64.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build gc
+
 package snappy
 
 // decode has the same semantics as in decode_other.go.
